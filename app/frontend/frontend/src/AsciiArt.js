eaa536b979f6332659a4d11b062dbdaf5f358cc8
Noah <burghardtnoah@gmail.com>
2021-01-24 19:51:23 +0000

    displays response text

--- app/frontend/frontend/src/AsciiArt.js
+++ app/frontend/frontend/src/AsciiArt.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react'
 
+const AsciiDisplay = (props) => {
+    return (
+        <div>
+            <p>
+                {props.text}
+            </p>
+        </div>
+    )
+}
 class AsciiArt extends Component {
     state = {
         width: 150,
         invert: true,
-        braille: true
+        braille: true,
+        text: ""
     }
+
     fileInput = React.createRef();
+
     handleSubmit = (event) => {
         event.preventDefault();
 
@@ -19,20 +31,30 @@ class AsciiArt extends Component {
         fetch('http://docker/api/ascii-demo/', {
             method: 'POST',
             body: data
-        }).then(function(response){
-            console.log(response.text())
+        }).then((response) => {
+            response.text().then((text) => {
+                this.setState({
+                    text: text
+                });
+            })
         });
     }
 
     handleInputChange = (event) => {
         const target = event.target;
-        const value = target.type ==='checkbox' ? target.checked : target.value;
+        let value = target.value;
+        if (target.type === "number"){
+            value = parseInt(target.value);
+        }
+        else if (target.type === "checkbox"){
+            value = target.checked
+        }
         const name = target.name;
+
         this.setState({
             [name]: value
         });
     }
-
 
     render() { return (
         <div className="demo">
@@ -40,13 +62,14 @@ class AsciiArt extends Component {
             <label htmlFor="file">Upload an Image: </label>
             <input name="file" id="file" type="file" ref={this.fileInput} accept="image/png, image/jpeg"/><br/><br/>
             <label htmlFor="width">Width (in characters):</label>
-            <input type="number" step="1" defaultValue={this.state.width} onChange={this.handleInputChange}/><br/><br/>
+            <input type="number" name="width" step="1" defaultValue={this.state.width} onChange={this.handleInputChange}/><br/><br/>
             <input type="checkbox" id="invert" name="invert" checked={this.state.invert} onChange={this.handleInputChange}/>
             <label htmlFor="invert">Invert color</label><br/><br/>
             <input type="checkbox" id="braille" name="braille" checked={this.state.braille} onChange={this.handleInputChange}/>
             <label htmlFor="braille">Use braille characters</label><br/><br/>
             <input type="submit" value="submit"/>
             </form>
+            <AsciiDisplay text={this.state.text}/>
         </div>
     )}
 
