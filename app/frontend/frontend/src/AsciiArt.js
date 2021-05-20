5f49f4598815d1b5f583af85a706779b3c26c7f9
Noah <burghardtnoah@gmail.com>
2021-05-20 17:49:09 +0000

    added some robustness to ascii art thing

--- app/frontend/frontend/src/AsciiArt.js
+++ app/frontend/frontend/src/AsciiArt.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import configData from './config.json'
 
 const AsciiDisplay = (props) => {
     return (
@@ -14,16 +15,14 @@ class AsciiArt extends Component {
         braille: true,
         text: "",
         disabled: false,
-        replace: false
+        replace: false,
+        file_size: 0
     }
 
     fileInput = React.createRef();
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setState({
-            disabled: true
-        });
 
         const data = new FormData(event.target);
         data.set('width', this.state.width);
@@ -31,23 +30,65 @@ class AsciiArt extends Component {
         data.set('braille', this.state.braille);
         data.append('file', this.fileInput);
 
-        fetch('http://docker/api/ascii-demo/', {
+        // Ensure the file size is reasonable
+        if (this.state.file_size === 0){
+            this.setState({
+                text: "Error in reading image."
+            })
+            return;
+        }
+        else if (this.state.file_size > 2000000){
+            this.setState({
+                text: "File size too large. Please select a file smaller than 2.0 MB"
+            })
+            return;
+        }
+
+        // Disable the UI while we wait for the backend response
+        this.setState({
+            disabled: true
+        });
+
+        fetch(new URL('/api/ascii-demo/', configData.BACKEND_URL), {
+        //fetch('http://docker/api/ascii-demo/', {
             method: 'POST',
             body: data,
             mode: 'no-cors'
         }).then((response) => {
             response.text().then((text) => {
-                this.setState({
-                    text: text,
-                    disabled: false
-                });
-                if (this.state.replace && this.state.braille){
                     this.setState({
-                        text: this.state.text.replace(/⠀/g, "⠄")
+                        disabled: false
                     });
+                if (response.ok){
+                    this.setState({
+                        text: text
+                    });
+                    if (this.state.replace && this.state.braille){
+                        this.setState({
+                            text: this.state.text.replace(/⠀/g, "⠄")
+                        });
+                    }
+                }
+                else{
+                    this.setState({
+                        text: "Error: Try a different image or character width"
+                    })
                 }
             })
         });
+    }
+
+    handleFileChange = (event) => {
+        try{
+            this.setState({
+                file_size: event.target.files[0].size
+            });
+        }
+        catch (e) {
+            this.setState({
+                file_size: 0
+            });
+        }
     }
 
     handleInputChange = (event) => {
@@ -75,7 +116,7 @@ class AsciiArt extends Component {
             <fieldset disabled={this.state.disabled}>
             <form ref="form" onSubmit={this.handleSubmit}>
             <label htmlFor="file">Upload an Image: </label>
-            <input name="file" id="file" type="file" ref={this.fileInput} accept="image/png, image/jpeg"/><br/><br/>
+            <input name="file" id="file" type="file" ref={this.fileInput} accept="image/png, image/jpeg" onChange={this.handleFileChange}/><br/><br/>
             <label htmlFor="width">Width (in characters): </label>
             <input type="number" id="width" name="width" step="1" defaultValue={String(this.state.width)} onChange={this.handleInputChange}/><br/><br/>
             <input type="checkbox" id="invert" name="invert" checked={this.state.invert} onChange={this.handleInputChange}/>
