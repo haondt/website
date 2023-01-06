0f4ca5bf2461b5cdc949e1147e0c75c57a9503f8
Noah <burghardtnoah@gmail.com>
2023-01-06 15:58:28 -0700

    updated angular material to 15

--- app/frontend/frontend/src/app/app.module.ts
+++ app/frontend/frontend/src/app/app.module.ts
@@ -12,11 +12,11 @@ import { WalletComponent } from './library/wallet/wallet.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
+import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
